d6889e2d92678920a696b75e535bd171e375799d
Adrian Orioki <adrianorioki@gmail.com>
2023-02-25 19:34:16 +0300

    Create control.py

---
+++ Week 2/control.py
@@ -0,0 +1,10 @@
+for n in range(2, 89):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, 'is a prime number')
+
+
+    
